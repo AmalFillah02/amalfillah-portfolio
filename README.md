2e1477742f0cb5cc642e197f9ff845b68b6a3483
AmalFillah02 <amalfillah02@gmail.com>
2025-06-20 02:18:05 +0700

    Inital commit

---
+++ README.md
@@ -0,0 +1,27 @@
+### Description
+
+A simple portfolio built with React.
+
+### [live preview]()
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+- Edit Content From One Place
+
+### Setup
+
+Get the code
+
+<pre>git clone h</pre>
+
+Install required dependencies
+
+<pre>yarn install</pre>
+
+Start the server
+
+<pre>yarn start</pre>
---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "MY PORTFOLIO";
+const meta = {
+  title: "Ahsanul Amal Fillah",
+  description: "",
+};
+
+const introdata = {
+  title: "I’m Ahsanul Amal Fillah",
+  animated: {
+    first: "I love coding",
+    second: "I always learn more about code",
+    third: "I develop mobile apps too",
+  },
+  description:
+    "I am a versatile creator with a strong background in software development, computer networking, and graphic design. I love bringing ideas to life through innovative apps and compelling visual designs. Driven by curiosity and a growth mindset, I am always ready to push boundaries and make a meaningful impact in every project I take on.",
+  your_img_url: "https://i.imgur.com/UPSwAir.jpeg",
+};
+
+const dataabout = {
+  title: "A little about myself",
+  aboutme:
+    "A passion of me for developing websites and software. Over the years, I have honed my skills in creating dynamic web applications and building efficient programs. Additionally, I have a strong understanding of internet networking, which allows me to manage and optimize network systems effectively. I enjoy solving complex problems and continuously strive to expand my knowledge in technology, particularly in web development and networking.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Internship as a technician",
+    where: "CV. Soerya Djaya Com, Kudus",
+    date: "2020",
+  },
+  {
+    jobtitle: "Internship as a application developer",
+    where: "Kantor Kecamatan Keling, Jepara",
+    date: "2024",
+  },
+  {
+    jobtitle: "IT Staff",
+    where: "PT. Great Golden Indonesia",
+    date: "April - May 2025",
+  },
+  {
+    jobtitle: "Freelance Web Developer",
+    where: "Self-employed",
+    date: "2021 - 2025",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 85,
+  },
+  {
+    name: "HTML",
+    value: 95,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "CSS",
+    value: 90,
+  },
+  {
+    name: "C#",
+    value: 50,
+  },
+  {
+    name: "PHP",
+    value: 95,
+  },
+];
+
+const services = [
+  {
+    title: "Mobile Apps Developer",
+    description:
+      "As a mobile app developer, I’ve created various applications including games, AR experiences, and utility apps for both Android and iOS platforms. I focus on building engaging, user-friendly, and efficient mobile solutions.",
+  },
+  {
+    title: "Web Developer",
+    description:
+      "Providing website development services for various clients across different industries, including design, development, and maintenance of websites.",
+  },
+  {
+    title: "Network & Infrastructure Support",
+    description:
+      "With a solid understanding of computer networking, I offer services related to network setup, troubleshooting, and maintenance. I’ve handled tasks such as configuring routers, managing local networks, and ensuring stable internet connectivity, helping both individuals and organizations maintain reliable and secure network environments.",
+  },
+];
+
+//Atur Porto
+const dataportfolio = [
+  {
+    img: "https://i.imgur.com/Pil4uvg.jpeg",
+    description: "AR Museum Kartini Jepara Mobile APPS",
+    link: "https://ar-museum-kartini-jepara.id.uptodown.com/android",
+  },
+  {
+    img: "https://i.imgur.com/F2wJVeB.jpeg",
+    description: "Website Anugrah Furniture",
+    link: "https://amalfillah02.github.io/anugrah-furniture/",
+  },
+  {
+    img: "https://i.imgur.com/5gAVMsK.jpeg",
+    description: "Aplikasi Jadwal Kegiatan Sehari-hari",
+    link: "https://amalfillah02.github.io/app-kegiatan/",
+  },
+  {
+    img: "https://i.imgur.com/jdnFYyK.jpeg",
+    description: "Sistem Pengelolaan Buku",
+    link: "https://github.com/AmalFillah02/pengelolaan-buku",
+  },
+  {
+    img: "https://i.imgur.com/Q2DiyY7.jpeg",
+    description: "Sistem Aplikasi Dashboard Harga Crypto Realtime",
+    link: "https://github.com/AmalFillah02/dashboard-harga-crypto",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "amalfillah02@gmail.com.com",
+  YOUR_FONE: "+6282-138-199-126",
+  description:
+    "I am open to job opportunities, collaborations, or any professional inquiries related to my skills and experience. Please feel free to contact me — I would be glad to connect and contribute.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_xrzho4l",
+  YOUR_TEMPLATE_ID: "template_19np67c",
+  YOUR_USER_ID: "32XrYCPkqX9Jxa_hw",
+};
+
+const socialprofils = {
+  github: "https://github.com/AmalFillah02",
+  linkedin: "https://www.linkedin.com/in/ahsanul-amal-fillah",
+  instagram: "https://instagram.com/amalfillah_",
+  tiktok: "https://tiktok.com/@amalfillahhh",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
